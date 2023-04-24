a92f016cadf407b55bfd22c7711db144cac542f0
Andrew Weightman <andrewjw797@gmail.com>
2023-04-23 20:29:22 -0700

    ertdhn

--- src/lib/webSocketConnection.js
+++ src/lib/webSocketConnection.js
@@ -1,6 +1,6 @@
 import ioClient from 'socket.io-client';
 
-const ENDPOINT = 'http://localhost:3000';
+const ENDPOINT = 'https://tiktok-streammonitor-eventsmicroservice.azurewebsites.net/';
 const socket = ioClient(ENDPOINT, {
     extraHeaders: {
         "chat-traffic": "test"
